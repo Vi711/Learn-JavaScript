0b9d8c865ce5140c004ae52abb63df08dc257e0b
Victor Zavarin <Holdenrush@gmail.com>
2025-07-22 14:33:22 +0400

    Урок 3

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const lastname = prompt('Ваша фамилия?', '');
+const firstname = prompt('Ваше имя?', '');
+const secondname = prompt('Ваше отчество?', '');
+
+alert(`Добро пожаловать на сайт, ${lastname} ${firstname} ${secondname}`)
+console.log(`Добро пожаловать на сайт, ${lastname} ${firstname} ${secondname}`)
